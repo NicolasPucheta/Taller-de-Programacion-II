662c93865f0bbee423962d48eaf5e391285116a4
NicolasPucheta <nicopuche112005@gmail.com>
2025-08-16 19:55:50 -0300

    agregado del tp2

---
+++ Pequeño Formulario/Pequeño Formulario/Pequeño Formulario.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LNya = New System.Windows.Forms.Label()
+        Me.LDni = New System.Windows.Forms.Label()
+        Me.LApellido = New System.Windows.Forms.Label()
+        Me.LNombre = New System.Windows.Forms.Label()
+        Me.LModificar = New System.Windows.Forms.Label()
+        Me.TApellido = New System.Windows.Forms.TextBox()
+        Me.TNombre = New System.Windows.Forms.TextBox()
+        Me.TDni = New System.Windows.Forms.TextBox()
+        Me.TGuardar = New System.Windows.Forms.Button()
+        Me.TEliminar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LNya
+        '
+        Me.LNya.AutoSize = True
+        Me.LNya.Location = New System.Drawing.Point(68, 72)
+        Me.LNya.Name = "LNya"
+        Me.LNya.Size = New System.Drawing.Size(92, 13)
+        Me.LNya.TabIndex = 0
+        Me.LNya.Text = "Nombre y Apellido"
+        '
+        'LDni
+        '
+        Me.LDni.AutoSize = True
+        Me.LDni.Location = New System.Drawing.Point(68, 126)
+        Me.LDni.Name = "LDni"
+        Me.LDni.Size = New System.Drawing.Size(26, 13)
+        Me.LDni.TabIndex = 1
+        Me.LDni.Text = "DNI"
+        '
+        'LApellido
+        '
+        Me.LApellido.AutoSize = True
+        Me.LApellido.Location = New System.Drawing.Point(68, 161)
+        Me.LApellido.Name = "LApellido"
+        Me.LApellido.Size = New System.Drawing.Size(44, 13)
+        Me.LApellido.TabIndex = 2
+        Me.LApellido.Text = "Apellido"
+        '
+        'LNombre
+        '
+        Me.LNombre.AutoSize = True
+        Me.LNombre.Location = New System.Drawing.Point(68, 199)
+        Me.LNombre.Name = "LNombre"
+        Me.LNombre.Size = New System.Drawing.Size(44, 13)
+        Me.LNombre.TabIndex = 3
+        Me.LNombre.Text = "Nombre"
+        '
+        'LModificar
+        '
+        Me.LModificar.AutoSize = True
+        Me.LModificar.ForeColor = System.Drawing.Color.Red
+        Me.LModificar.Location = New System.Drawing.Point(208, 72)
+        Me.LModificar.Name = "LModificar"
+        Me.LModificar.Size = New System.Drawing.Size(49, 13)
+        Me.LModificar.TabIndex = 4
+        Me.LModificar.Text = "modificar"
+        '
+        'TApellido
+        '
+        Me.TApellido.Location = New System.Drawing.Point(177, 158)
+        Me.TApellido.Name = "TApellido"
+        Me.TApellido.Size = New System.Drawing.Size(100, 20)
+        Me.TApellido.TabIndex = 5
+        '
+        'TNombre
+        '
+        Me.TNombre.Location = New System.Drawing.Point(177, 196)
+        Me.TNombre.Name = "TNombre"
+        Me.TNombre.Size = New System.Drawing.Size(100, 20)
+        Me.TNombre.TabIndex = 6
+        '
+        'TDni
+        '
+        Me.TDni.Location = New System.Drawing.Point(177, 123)
+        Me.TDni.Name = "TDni"
+        Me.TDni.Size = New System.Drawing.Size(100, 20)
+        Me.TDni.TabIndex = 7
+        '
+        'TGuardar
+        '
+        Me.TGuardar.Location = New System.Drawing.Point(71, 291)
+        Me.TGuardar.Name = "TGuardar"
+        Me.TGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.TGuardar.TabIndex = 8
+        Me.TGuardar.Text = "Guardar"
+        Me.TGuardar.UseVisualStyleBackColor = True
+        '
+        'TEliminar
+        '
+        Me.TEliminar.Location = New System.Drawing.Point(202, 291)
+        Me.TEliminar.Name = "TEliminar"
+        Me.TEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.TEliminar.TabIndex = 9
+        Me.TEliminar.Text = "Eliminar"
+        Me.TEliminar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 411)
+        Me.Controls.Add(Me.TEliminar)
+        Me.Controls.Add(Me.TGuardar)
+        Me.Controls.Add(Me.TDni)
+        Me.Controls.Add(Me.TNombre)
+        Me.Controls.Add(Me.TApellido)
+        Me.Controls.Add(Me.LModificar)
+        Me.Controls.Add(Me.LNombre)
+        Me.Controls.Add(Me.LApellido)
+        Me.Controls.Add(Me.LDni)
+        Me.Controls.Add(Me.LNya)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LNya As Label
+    Friend WithEvents LDni As Label
+    Friend WithEvents LApellido As Label
+    Friend WithEvents LNombre As Label
+    Friend WithEvents LModificar As Label
+    Friend WithEvents TApellido As TextBox
+    Friend WithEvents TNombre As TextBox
+    Friend WithEvents TDni As TextBox
+    Friend WithEvents TGuardar As Button
+    Friend WithEvents TEliminar As Button
+End Class
---
+++ Pequeño Formulario/Pequeño Formulario/Pequeño Formulario.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    ' Validación para TDni (solo números)
+    Private Sub TDni_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TDni.KeyPress
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    ' Validación para TApellido (solo letras)
+    Private Sub TApellido_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TApellido.KeyPress
+        If Not Char.IsLetter(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    ' Validación para TNombre (solo letras)
+    Private Sub TNombre_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TNombre.KeyPress
+        If Not Char.IsLetter(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub TGuardar_Click(sender As Object, e As EventArgs) Handles TGuardar.Click
+        ' 8) Crea la variable para el resultado del MsgBox.
+        Dim ask As MsgBoxResult
+        ' 7) Valida que todos los campos estén completos.
+        If TDni.Text <> "" AndAlso TApellido.Text <> "" AndAlso TNombre.Text <> "" Then
+            LModificar.Text = TNombre.Text & " " & TApellido.Text
+            ' 9) Muestra el mensaje de consulta y asigna el resultado a la variable "ask".
+            ' El MsgBox tiene botones Si y No, un ícono de pregunta y el foco en el botón "Si".
+            ask = MessageBox.Show("¿Seguro que desea insertar un nuevo Cliente?", "Confirmar Insercion", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1)
+
+            ' Usa la sentencia if para verificar si el usuario presionó "Si".
+            If ask = MsgBoxResult.Yes Then
+                ' 10) Muestra un mensaje de información con el nombre completo del cliente.
+                Dim nombreCompleto As String = TApellido.Text & " " & TNombre.Text
+                MessageBox.Show("El Cliente: " & nombreCompleto & " se insertó Correctamente", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        Else
+            ' Si algún campo está incompleto, muestra un mensaje de error crítico.
+            MessageBox.Show("Debe Completar todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub TEliminar_Click(sender As Object, e As EventArgs) Handles TEliminar.Click
+        ' Declara la variable para el resultado del MsgBox
+        Dim resultado As MsgBoxResult
+        Dim nombreCompleto As String = TApellido.Text & " " & TNombre.Text
+
+        ' 11) Muestra el mensaje de advertencia con el foco en "No"
+        resultado = MessageBox.Show("Está apunto de eliminar el Cliente: " & nombreCompleto, "Confirmar Eliminacion", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2)
+
+        ' 12) Verifica la respuesta del usuario
+        If resultado = MsgBoxResult.Yes Then
+            ' Si la respuesta es "Sí", muestra el mensaje de confirmación
+            MessageBox.Show("El Cliente: " & nombreCompleto & " se eliminó correctamente", "Eliminar", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            ' Limpia todos los campos de texto
+            TDni.Clear()
+            TApellido.Clear()
+            TNombre.Clear()
+            LModificar.Text = "" ' Limpia el label LModificar
+        End If
+        ' Si la respuesta es "No", no sucede nada (el código simplemente termina)
+    End Sub
+End Class
